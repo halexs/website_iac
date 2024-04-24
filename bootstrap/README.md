aabd50177e3562e40b391c2840aa49322f355b7d
Alex Shay <a.hsu94@gmail.com>
2024-04-23 21:31:07 -0400

    Create bootstrap directory for state

---
+++ bootstrap/README.md
@@ -0,0 +1,6 @@
+Bootstrap code sets the environment ready to be automated.
+
+Will eventually do: 
+- create initial IAM roles and users
+- create terraform state s3 bucket (while this state is managed in this directory)
+- setup admin accounts
---
+++ bootstrap/main.tf
@@ -0,0 +1,28 @@
+
+terraform {
+  backend "local" {
+    path = "./terraform.state"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "state" {
+  bucket = "kaelnomad-terraform-state"
+
+  tags = {
+    Name        = "State Bucket"
+  }
+
+#   force_destroy = true
+}
